029041a28d832dea1cffc28f49c5e2a2179f11b1
Saksham Mittal <saksham1991999@users.noreply.github.com>
2020-12-24 22:40:36 +0530

    spotlight admin models registered

--- spotlight/admin.py
+++ spotlight/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+
+admin.site.register(models.Category)
+admin.site.register(models.Post)
+admin.site.register(models.PostReact)
+admin.site.register(models.PostImage)
+admin.site.register(models.PostFile)
+admin.site.register(models.PostComment)
+admin.site.register(models.PostCommentReply)
