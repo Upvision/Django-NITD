9aa5c6100b097a187e7b21d5872b4d46e4718136
Prem_3081 <56423583+Prem1803@users.noreply.github.com>
2020-12-25 12:51:06 +0530

    updated social app

--- social/views.py
+++ social/views.py
@@ -6,3 +6,66 @@ from django.db.models import Q
 
 from social import models, serializers
 # Create your views here.
+
+
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def get_queryset(self):
+        query_set = models.Post.objects.all()
+        search = self.request.query_params.get('search', None)
+        if search:
+            query_set = query_set.filter(description__icontains=search)
+
+        order_by = self.request.query_params.get('orderby', None)
+        if order_by:
+            if order_by == "recent":
+                query_set = query_set.orderby('-date')
+
+        return query_set
+
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+
+class PostReactViewSet(viewsets.ModelViewSet):
+    queryset = models.PostReact.objects.all()
+    serializer_class = serializers.PostReactSerializer
+
+    def get_permissions(self):
+        if self.action in ('list', 'create'):
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+
+class PostCommentViewSet(viewsets.ModelViewSet):
+    queryset = models.PostComment.objects.all()
+    serializer_class = serializers.PostCommentSerializer
+
+    def get_permissions(self):
+        if self.action in ('list', 'create'):
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+
+class PostCommentReplyViewSet(viewsets.ModelViewSet):
+    queryset = models.PostCommentReply.objects.all()
+    serializer_class = serializers.PostCommentReplySerializer
+
+    def get_permissions(self):
+        if self.action in ('list', 'create'):
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAdminUser]
+        return [permission() for permission in permission_classes]
